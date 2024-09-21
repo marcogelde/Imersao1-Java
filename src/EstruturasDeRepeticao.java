6a39cc3069b7e186c334b4ffbe69aaaec9f42f60
Marco Antonio Gelde <marcogelde@hotmail.com>
2024-09-21 11:35:45 -0300

    EstruturasDeRepeticao

---
+++ src/EstruturasDeRepeticao.java
@@ -0,0 +1,17 @@
+public class EstruturasDeRepeticao {
+    public static void main(String[] args) {
+        // Exemplo com for
+        for (int i = 0; i < 5; i++) {
+            System.out.println("Número: " + i);
+        }
+
+        // Exemplo com while
+        int contador = 0;
+        while (contador < 5) {
+            System.out.println("Contador: " + contador);
+            contador++;
+        }
+
+        // To be continued
+    }
+}
