3c026ad56995a549e4923773842c79988461d1f8
Marco Antonio Gelde <marcogelde@hotmail.com>
2024-09-23 21:51:00 -0300

    Lista de fruta

--- src/ListaDeFrutas.java
+++ src/ListaDeFrutas.java
@@ -20,53 +20,72 @@ public class ListaDeFrutas {
 
             switch (opcao) {
                 case 1:
-                    if (quantidade == 0) {
-                        System.out.println("A lista está vazia.");
-                    } else {
-                        System.out.println("Lista de frutas:");
-                        for (int i = 0; i < quantidade; i++) {
-                            System.out.println((i + 1) + "ª fruta: " + frutas[i]);
-                        }
-                    }
+                    Listar(quantidade, frutas);
                     break;
                 case 2:
-                    if (quantidade == 5) {
-                        System.out.println("A lista já está cheia. Não é possível adicionar mais frutas.");
-                    } else {
-                        System.out.print("Digite o nome da fruta: ");
-                        frutas[quantidade] = scanner.nextLine();
-                        quantidade++;
-                    }
+                    quantidade = Adicionar(quantidade, frutas, scanner);
                     break;
                 case 3:
-                    if (quantidade == 0) {
-                        System.out.println("A lista está vazia. Não há frutas para remover.");
-                    } else {
-                        System.out.println("Digite o número da fruta que deseja remover:");
-                        for (int i = 0; i < quantidade; i++) {
-                            System.out.println((i + 1) + "ª fruta: " + frutas[i]);
-                        }
-                        int remover = scanner.nextInt() - 1;
-
-                        if (remover >= 0 && remover < quantidade) {
-                            // Deslocar os elementos para a esquerda
-                            for (int i = remover; i < quantidade - 1; i++) {
-                                frutas[i] = frutas[i + 1];
-                            }
-                            frutas[quantidade - 1] = null; // Opcional: limpar a última posição
-                            quantidade--;
-                            System.out.println("Fruta removida com sucesso.");
-                        } else {
-                            System.out.println("Opção inválida.");
-                        }
-                    }
+                    quantidade = Remover(quantidade, frutas, scanner);
                     break;
                 default:
-                    System.out.println("Programa finalizado.");
+                    Finalizar();
                     return;
             }
         } while (opcao > 0 && opcao < 4);
 
         scanner.close();
+    }
+
+    private static void Finalizar() {
+        System.out.println("Programa finalizado.");
+        return;
+    }
+
+    private static int Remover(int quantidade, String[] frutas, Scanner scanner) {
+        if (quantidade == 0) {
+            System.out.println("A lista está vazia. Não há frutas para remover.");
+        } else {
+            System.out.println("Digite o número da fruta que deseja remover:");
+            for (int i = 0; i < quantidade; i++) {
+                System.out.println((i + 1) + "ª fruta: " + frutas[i]);
+            }
+            int remover = scanner.nextInt() - 1;
+
+            if (remover >= 0 && remover < quantidade) {
+                // Deslocar os elementos para a esquerda
+                for (int i = remover; i < quantidade - 1; i++) {
+                    frutas[i] = frutas[i + 1];
+                }
+                frutas[quantidade - 1] = null; // Opcional: limpar a última posição
+                quantidade--;
+                System.out.println("Fruta removida com sucesso.");
+            } else {
+                System.out.println("Opção inválida.");
+            }
+        }
+        return quantidade;
+    }
+
+    private static int Adicionar(int quantidade, String[] frutas, Scanner scanner) {
+        if (quantidade == 5) {
+            System.out.println("A lista já está cheia. Não é possível adicionar mais frutas.");
+        } else {
+            System.out.print("Digite o nome da fruta: ");
+            frutas[quantidade] = scanner.nextLine();
+            quantidade++;
+        }
+        return quantidade;
+    }
+
+    private static void Listar(int quantidade, String[] frutas) {
+        if (quantidade == 0) {
+            System.out.println("A lista está vazia.");
+        } else {
+            System.out.println("Lista de frutas:");
+            for (int i = 0; i < quantidade; i++) {
+                System.out.println((i + 1) + "ª fruta: " + frutas[i]);
+            }
+        }
     }
 }
